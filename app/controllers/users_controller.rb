5813e00ff1f47ab84a7890250b07a200f5cd5b3f
To-Ka <kobayashitoshifumi@kobayashitoshifuminoMacBook-Air.local>
2020-01-27 20:42:42 +0900

    create no resources

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,35 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    User.create(user_params)
+  end
+
+  def index
+    @users = User.all
+  end
+
+  def destroy
+    user_data = User.find(params[:id])
+    user_data.delete
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    user_data = User.find(params[:id])
+    user_data.update(user_params)
+  end
+
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :age, :email)
+  end
+
+end
